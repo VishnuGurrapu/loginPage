dcc928c0c36a7364f216cd0d542f39232a040476
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-08-28 15:47:12 +0000

    error msg remained

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+//<Route exact path="/cart" component={Cart} />
+//<Route exact path="/products" component={Products} />
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+//import Products from './components/Products'
+//import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+      
+      
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
