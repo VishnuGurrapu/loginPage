243ea12b9a6366dfe7b5fb6d98f1fe0446f0933c
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-08-29 17:34:48 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-//<Route exact path="/cart" component={Cart} />
-//<Route exact path="/products" component={Products} />
+// <Route exact path="/cart" component={Cart} />
+// <Route exact path="/products" component={Products} />
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
-//import Products from './components/Products'
-//import Cart from './components/Cart'
+// import Products from './components/Products'
+// import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      
-      
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
